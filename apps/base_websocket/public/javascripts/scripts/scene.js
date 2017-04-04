70d410a7216eccba026cf9fd1ec66ecb98bec359
alexianM <alexian.masson@gmail.com>
2017-04-04 11:45:50 +0200

    CLEAN: typo + indent

--- apps/base_websocket/public/javascripts/scripts/scene.js
+++ apps/base_websocket/public/javascripts/scripts/scene.js
@@ -43,17 +43,17 @@ function initContainer() {
   var material = new THREE.LineBasicMaterial({color: 'green'});   
 
   for ( var i = - SIZE; i <= SIZE; i += step) {
-      geometry.vertices.push(new THREE.Vector3( - SIZE, 0, i ));
+    geometry.vertices.push(new THREE.Vector3( - SIZE, 0, i ));
       geometry.vertices.push(new THREE.Vector3( SIZE, 0, i ));
 
-      geometry.vertices.push(new THREE.Vector3( i, 0, - SIZE ));
-      geometry.vertices.push(new THREE.Vector3( i, 0, SIZE ));
+    geometry.vertices.push(new THREE.Vector3( i, 0, - SIZE ));
+    geometry.vertices.push(new THREE.Vector3( i, 0, SIZE ));
 
-      geometry.vertices.push(new THREE.Vector3( i, - SIZE, 0 ));
-      geometry.vertices.push(new THREE.Vector3( i, SIZE, 0 ));
+    geometry.vertices.push(new THREE.Vector3( i, - SIZE, 0 ));
+    geometry.vertices.push(new THREE.Vector3( i, SIZE, 0 ));
 
-      geometry.vertices.push(new THREE.Vector3( - SIZE, i, 0 ));
-      geometry.vertices.push(new THREE.Vector3( SIZE, i, 0 ));
+    geometry.vertices.push(new THREE.Vector3( - SIZE, i, 0 ));
+    geometry.vertices.push(new THREE.Vector3( SIZE, i, 0 ));
   }
 
   var line = new THREE.Line( geometry, material, THREE.LinePieces );
@@ -61,7 +61,7 @@ function initContainer() {
 
   // Cube
   var geometry = new THREE.BoxGeometry( CUBE_SIDE, CUBE_SIDE, CUBE_SIDE );
-	var material =   new THREE.MeshBasicMaterial({ color: 'yellow' });
+  var material =   new THREE.MeshBasicMaterial({ color: 'yellow' });
   cube = new THREE.Mesh( geometry, material ); 
   cube.position.x = 0;
   cube.position.y = 10;
@@ -120,7 +120,7 @@ function update() {
   if(followCamMode == 1)
   {
     // follow camera at each frame
-    lookAt(cube.position);
+    camera.lookAt(cube.position);
   }
   else if (followCamMode == 2)
   {
@@ -128,7 +128,7 @@ function update() {
     var time = Date.now();
 
     if (time > previousTime + 1000) { // 1 seconds refresh rates
-          lookAt(cube.position);
+      camera.lookAt(cube.position);
       previousTime = time;
     }
   }
@@ -146,14 +146,14 @@ var graph = new SmoothieChart();
 graph.streamTo(document.getElementById('graphCanvas'));
 
 var rotLines = {
-    x: new TimeSeries(),
-    y: new TimeSeries(),
-    z: new TimeSeries()
+  x: new TimeSeries(),
+  y: new TimeSeries(),
+  z: new TimeSeries()
 };
 
- graph.addTimeSeries(rotLines.x, {strokeStyle: '#ff0000'});
- graph.addTimeSeries(rotLines.y, {strokeStyle: '#00ff00'});
- graph.addTimeSeries(rotLines.z, {strokeStyle: '#0000ff'});
+graph.addTimeSeries(rotLines.x, {strokeStyle: '#ff0000'});
+graph.addTimeSeries(rotLines.y, {strokeStyle: '#00ff00'});
+graph.addTimeSeries(rotLines.z, {strokeStyle: '#0000ff'});
 
 /*
  *
@@ -177,34 +177,33 @@ hub.on("solution", function (event)  {
 
 
 hub.on("pingScene", function (event)  {
-    if(event.return){
-        $(document).trigger("add-alerts", {
-          message: "Connexion établi avec le mobile",
-          priority: "info"
-        });
+  if(event.return){
+    $(document).trigger("add-alerts", {
+      message: "Connexion établi avec le mobile",
+      priority: "info"
+    });
 
-        hub.emit("pingMobile", {
-                msg : "ok"
-            });
-    }
+    hub.emit("pingMobile", {
+      msg : "ok"
+    });
+  }
 });
 
 hub.on("noSupported", function (event)  {
-    $(document).trigger("add-alerts", {
-      message: "Le navigateur ne supporte pas le capteur suivant "+event.type,
-      priority: "error"
-    });
+  $(document).trigger("add-alerts", {
+    message: "Le navigateur ne supporte pas le capteur suivant "+event.type,
+    priority: "error"
+  });
 });
 
 hub.on("deviceOrientation", function (event) {
-    cube.rotation.x = 2 * Math.PI * event.beta / 360;
-    cube.rotation.y = 2 * Math.PI * event.gamma / 360;
-    cube.rotation.z = 2 * Math.PI * event.alpha / 360;
+  cube.rotation.x = 2 * Math.PI * event.beta / 360;
+  cube.rotation.y = 2 * Math.PI * event.gamma / 360;
+  cube.rotation.z = 2 * Math.PI * event.alpha / 360;
 
-    rotLines.x.append(new Date().getTime(), event.beta);
-    rotLines.y.append(new Date().getTime(), event.gamma);
-    rotLines.z.append(new Date().getTime(), event.alpha);
-
+  rotLines.x.append(new Date().getTime(), event.beta);
+  rotLines.y.append(new Date().getTime(), event.gamma);
+  rotLines.z.append(new Date().getTime(), event.alpha);
 });
 
 
@@ -216,16 +215,7 @@ vitesse.y = 0;
 vitesse.z = 0;
 
 hub.on("deviceMotion", function (event) {
-     //console.log(event)
-     if(solutionNumber == 1){
-
-         vitesse.x =  deltat*event.acceleration.x;
-         vitesse.y = deltat*event.acceleration.y;
-         vitesse.z = deltat*event.acceleration.z;
-         cube.position.x = cube.position.x + deltat*vitesse.x;
-         cube.position.y = cube.position.y + deltat*vitesse.y;
-         cube.position.z = cube.position.z + deltat*vitesse.z;
-     }
+  //console.log(event)
 });
 
  
